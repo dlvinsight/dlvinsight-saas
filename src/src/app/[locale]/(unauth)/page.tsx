824cf87ac84d20fb1943ba53ca987f2c6a2c12ae
DLVInsight <data+git@dlvinsight.com>
2025-06-22 11:21:24 +0200

    Remove unused Features import to fix build error

--- src/src/app/[locale]/(unauth)/page.tsx
+++ src/src/app/[locale]/(unauth)/page.tsx
@@ -3,7 +3,6 @@ import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import { CTA } from '@/templates/CTA';
 import { DemoBanner } from '@/templates/DemoBanner';
 import { FAQ } from '@/templates/FAQ';
-import { Features } from '@/templates/Features';
 import { Footer } from '@/templates/Footer';
 import { Hero } from '@/templates/Hero';
 import { Navbar } from '@/templates/Navbar';
